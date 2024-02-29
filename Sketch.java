913737229c095951842d8287e46749260c44ad50
Link1400 <ties_derby0d@icloud.com>
2024-02-29 00:31:51 -0500

    finished project

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,10 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+  //Define Variables below for window size
+ 
+
+    size(800, 800);
   }
 
   /** 
@@ -16,21 +19,36 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(0, 0, 255);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+/**
+color(c1) = #FFFF00;
+color(c2) = #00FF00;
+colorc3 = #0000FF;
+color c4 = #964B00;
+*/
+//float widthfloat = (float)width;
+//float heightfloat = (float)height;
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    fill(218, 160, 109);
+    ellipse(height/2,width/2,height - 100,width - 100);
+    fill(165, 42, 42);
+    fill(218, 160, 109);
+    
+
+    fill(255,255,0);
+    ellipse(height-(height-100), width/2, height - (height-100), width - (width - 100));
+    ellipse(height - 100,width/2,height - (height-100),width - (width-100));
+    
+
   
-  // define other methods down here.
+
 }
+
+
+  }
